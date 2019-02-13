2fbe8fa1457738b57bcdec75bfe4630c988dfce3
nathanferrell <46502922+nathanferrell@users.noreply.github.com>
2019-02-13 09:24:13 -0500

    Update konami.js

--- konami.js
+++ konami.js
@@ -36,7 +36,7 @@ var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 	+ minutes + "m " + seconds + "s ";
 	
 	// If the count down is over, write some text 
-	if (distance < 107) {
+	if (distance < 150) {
 		document.getElementById("celebrate").innerHTML = "WE ARE ALMOST THERE";
 	}
 	if (distance < 0) {
