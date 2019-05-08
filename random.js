eff39cb635425d96b9372c2f8bc9785003d8ee9f
Matthew C. Gardner <matthewgardner@ccs.k12.in.us>
2019-05-08 12:39:28 -0400

    ogjaosgh

--- random.js
+++ random.js
@@ -1,2 +1,27 @@
-var a = Math.seedrandom("test");
-console.log(a);
+// in order to work 'Math.seed' must NOT be undefined,
+// so in any case, you HAVE to provide a Math.seed
+Math.seededRandom = function(max, min) {
+    max = max || 1;
+    min = min || 0;
+ 
+    Math.seed = (Math.seed * 9301 + 49297) % 233280;
+    var rnd = Math.seed / 233280;
+ 
+    return min + rnd * (max - min);
+}
+
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + dd + yyyy;
+
+Math.seed = today;
+randomNum = Math.round(Math.seededRandom() * 1000000);
+randomNum = Array.from(randomNum.toString()).map(Number);
+console.log(randomNum);
+
+// Copy from templates based on the numbers
+// Pick template based on number in array.
