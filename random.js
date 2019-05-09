2ff4c1b76eb68b27480c6511811a3bd9427d00d0
Matthew C. Gardner <matthewgardner@ccs.k12.in.us>
2019-05-09 09:23:11 -0400

    sghaklg

--- random.js
+++ random.js
@@ -25,3 +25,44 @@ console.log(randomNum);
 
 // Copy from templates based on the numbers
 // Pick template based on number in array.
+
+var totalTemplates = 2;
+var totalSpritesheets = 3;
+var topLeftChoice = randomNum[0];
+var topRightChoice = randomNum[1];
+var bottomLeftChoice = randomNum[2];
+var bottomRightChoice = randomNum[3];
+var spriteChoice = randomNum[4];
+
+var topLeftTemplate = topLeftChoice % totalTemplates;
+var topRightTemplate = topRightChoice % totalTemplates;
+var bottomLeftTemplate = bottomLeftChoice % totalTemplates;
+var bottomRightTemplate = bottomRightChoice % totalTemplates;
+var spriteTemplate = spriteChoice % totalSpritesheets;
+
+var tlTemplates = ['TL1', 'TL2'];
+var blTemplates = ['BL1', 'BL2'];
+var trTemplates = ['TR1', 'TR2'];
+var brTemplates = ['BR1', 'BR2'];
+console.log(topLeftTemplate, topRightTemplate, bottomLeftTemplate, bottomRightTemplate, spriteTemplate);
+
+
+GenerateRandomLevel() {
+	var canvas = document.getElementById("test1"),
+	ctx = canvas.getContext("2d");
+	ctx.drawImage(tlTemplates[topLeftTemplate], 0, 0, 32, 16, 0, 0, 32, 16);
+	ctx.drawImage(blTemplates[bottomLeftTemplate], 0, 0, 32, 16, 32, 0, 32, 16);
+	ctx.drawImage(trTemplates[topRightTemplate], 0, 0, 32, 16, 0, 16, 32, 16);
+	ctx.drawImage(brTemplates[bottomLeftTemplate], 0, 0, 32, 16, 32, 16, 32, 16);
+	GenerateLevel();
+	if (sessionStorage.getItem("coinArray") !== null) {
+		coin = JSON.parse(sessionStorage.getItem("coinArray"));
+		coinsCollected = sessionStorage.getItem("coinsCollected");
+		coinsNeeded = sessionStorage.getItem("coinsNeeded");
+	}
+	if (sessionStorage.getItem("checkpointArray") !== null) {
+		checkpoint = JSON.parse(sessionStorage.getItem("checkpointArray"));
+	}
+}
+
+
