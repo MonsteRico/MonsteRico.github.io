e73d0b17b67d00dd41b9d0cf9fb97cc672563bea
Matthew C. Gardner <matthewgardner@ccs.k12.in.us>
2020-01-16 17:41:08 -0500

    pagj

--- draw/html5-canvas-drawing-app.js
+++ draw/html5-canvas-drawing-app.js
@@ -1,4 +1,5 @@
-context = document.getElementById('canvas').getContext("2d");
+var context = document.getElementById('canvas').getContext("2d");
+var globalColor = "#df4b26";
 $('#canvas').mousedown(function(e){
   var mouseX = e.pageX - this.offsetLeft;
   var mouseY = e.pageY - this.offsetTop;
@@ -50,6 +51,7 @@ document.getElementById("canvas").addEventListener("touchend", function(e){
 var clickX = new Array();
 var clickY = new Array();
 var clickDrag = new Array();
+var clickColor = new Array();
 var paint;
 
 function addClick(x, y, dragging)
@@ -57,13 +59,18 @@ function addClick(x, y, dragging)
   clickX.push(x);
   clickY.push(y);
   clickDrag.push(dragging);
+  clickColor.push(globalColor);
 }
 function redraw(){
  context.width = window.innerWidth-15;
  context.height = window.innerHeight-15;
+  document.getElementById("canvas").width = window.innerWidth-15;
+ document.getElementById("canvas").height = window.innerHeight-15;
+ document.getElementById("div").style.width = window.innerWidth-15;
+ document.getElementById("div").style.height = window.innerHeight-15;
   context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
   context.drawImage(document.getElementById("img"), 0, 0, window.innerWidth-15, window.innerHeight-15);
-  context.strokeStyle = "#df4b26";
+  
   context.lineJoin = "round";
   context.lineWidth = 5;
 			
@@ -76,6 +83,42 @@ function redraw(){
      }
      context.lineTo(clickX[i], clickY[i]);
      context.closePath();
+	 context.strokeStyle = clickColor[i];
      context.stroke();
   }
+}
+
+function showTools() {
+	document.getElementById("canvas").style.display = "none";
+	document.getElementById("blue").style.display = "block";
+	document.getElementById("gold").style.display = "block";
+	document.getElementById("red").style.display = "block";
+	document.getElementById("button").style.display = "none";
+	document.getElementById("div").style.display = "block";
+}
+
+function hideTools() {
+		document.getElementById("canvas").style.display = "block";
+	document.getElementById("button").style.display = "block";
+	document.getElementById("div").style.display = "none";
+	document.getElementById("blue").style.display = "none";
+	document.getElementById("gold").style.display = "none";
+	document.getElementById("red").style.display = "none";
+}
+
+function setColor(color) {
+	switch (color) {
+		case "blue": {
+			globalColor = "#0f64fa"; 
+			break;
+		}
+		case "red": {
+			globalColor = "#df4b26";
+			break;
+		}
+		case "gold": {
+			globalColor = "#fac805";
+			break;
+		}
+	}
 }
