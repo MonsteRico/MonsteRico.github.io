bba08a7aa57891222ac66584e712af8816ce94cd
MonsteRico <32555072+MonsteRico@users.noreply.github.com>
2020-01-27 08:28:16 -0500

    Update enemies.js

--- testGame2/enemies.js
+++ testGame2/enemies.js
@@ -1,14 +1,4 @@
 class enemy {
-  constructor(x, y, color, health) {
-    this.x = x - 16;
-    this.y = y - 16;
-    this.width = 32;
-    this.height = 32;
-    this.opacity = 1;
-    this.color = color;
-    this.speed = 2;
-    this.health = health;
-  }
   constructor(x, y, color) {
     this.x = x - 16;
     this.y = y - 16;
